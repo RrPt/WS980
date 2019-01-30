d8844819ef5cb811c563892ecbb771ab161255b9
Rainer <R.Petzoldt@web.de>
2019-01-30 23:49:21 +0100

    Abrufen der Netzwerkparameter und Name tut

--- WS980/Form1.Designer.cs
+++ WS980/Form1.Designer.cs
@@ -28,13 +28,50 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tBOut = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tBOut
+            // 
+            this.tBOut.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tBOut.Location = new System.Drawing.Point(12, 42);
+            this.tBOut.Multiline = true;
+            this.tBOut.Name = "tBOut";
+            this.tBOut.Size = new System.Drawing.Size(774, 423);
+            this.tBOut.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(798, 477);
+            this.Controls.Add(this.tBOut);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.button1_Click);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox tBOut;
     }
 }
 
--- WS980/Form1.cs
+++ WS980/Form1.cs
@@ -4,6 +4,8 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,6 +17,29 @@ namespace WS980
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            GetComParameter();
+        }
+
+        private void GetComParameter()
+        {
+            var sendClient = new UdpClient("192.168.22.255",46000);
+            sendClient.Send(new byte[] { 0xff, 0xff, 0x12, 0x00, 0x04, 0x16 }, 6);
+            IPEndPoint lep = (IPEndPoint)sendClient.Client.LocalEndPoint;
+            sendClient.Close();
+
+
+            var receiveClient = new UdpClient(lep.Port);
+            IPEndPoint RemoteIpEndPoint = lep ;// new IPEndPoint(IPAddress.Any, 0);
+
+            //// Blocks until a message returns on this socket from a remote host.
+            Byte[] receiveBytes = receiveClient.Receive(ref RemoteIpEndPoint);
+            String outstr = BitConverter.ToString(receiveBytes);
+            tBOut.AppendText(outstr + Environment.NewLine);
+
         }
     }
 }
